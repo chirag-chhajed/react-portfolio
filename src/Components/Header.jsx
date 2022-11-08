28f36b7de6dfbc9f4c2b1443050c0aa300c47435
Chirag Chhajed <chhajedchirag18@gmail.com>
2022-11-08 18:20:44 +0530

    base files added

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header>
+            <img className="red" src="./public/11.png" alt="logo"  />
+        </header>
+    )
+}
